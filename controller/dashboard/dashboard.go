7d5c4a3284def9dd6f24a8aeba23b6cb51287035
HaoXuan chen <1575197604@qq.com>
2022-12-05 14:01:45 +0800

    fix

--- controller/dashboard/dashboard.go
+++ controller/dashboard/dashboard.go
@@ -1,11 +1,14 @@
 package dashboard
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"go_gateway/common/lib"
 	"go_gateway/dao"
 	"go_gateway/dto"
 	"go_gateway/middleware"
+	"go_gateway/public"
+	"time"
 )
 
 type DashboardController struct {
@@ -14,6 +17,8 @@ type DashboardController struct {
 func ServiceRegister(router *gin.RouterGroup) {
 	service := &DashboardController{}
 	router.GET("/panel_group_data", service.PanelGroupData)
+	router.GET("/flow_stat_data", service.FlowStatData)
+	router.GET("/pie_chart_data", service.PieChartData)
 }
 
 func (service *DashboardController) PanelGroupData(c *gin.Context) {
@@ -34,6 +39,54 @@ func (service *DashboardController) PanelGroupData(c *gin.Context) {
 		AppNum:          10,
 		TodayRequestNum: 10,
 		CurrentQPS:      10,
+	}
+	middleware.ResponseSuccess(c, out)
+}
+
+func (service *DashboardController) FlowStatData(c *gin.Context) {
+	out := &dto.ServiceStaticOutput{}
+	out.Title = "今日流量统计"
+	todayList := []int{}
+	for i := 0; i < time.Now().Hour(); i++ {
+		todayList = append(todayList, 1)
+	}
+	out.Today = todayList
+	yesterdayList := []int{}
+	for i := 0; i < 23; i++ {
+		yesterdayList = append(yesterdayList, 2)
+	}
+	out.Yesterday = yesterdayList
+	middleware.ResponseSuccess(c, out)
+}
+
+func (service *DashboardController) PieChartData(c *gin.Context) {
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+
+	serviceInfo := &dao.ServiceInfo{}
+	list, err := serviceInfo.GroupByLoadType(c, tx)
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+
+	legend := []string{}
+	for index, item := range list {
+		name, ok := public.LoadTypeMap[item.LoadType]
+		if !ok {
+			middleware.ResponseError(c, 2003, errors.New("load_type not found"))
+			return
+		}
+		list[index].Name = name
+		legend = append(legend, name)
+	}
+
+	out := &dto.DashServiceStatOutput{
+		Legend: legend,
+		Data:   list,
 	}
 	middleware.ResponseSuccess(c, out)
 }
--- dao/service_info.go
+++ dao/service_info.go
@@ -101,3 +101,13 @@ func (t *ServiceInfo) Find(c *gin.Context, tx *gorm.DB, search *ServiceInfo) (*S
 func (t *ServiceInfo) Save(c *gin.Context, tx *gorm.DB) error {
 	return tx.SetCtx(public.GetGinTraceContext(c)).Save(t).Error
 }
+
+func (t ServiceInfo) GroupByLoadType(c *gin.Context, tx *gorm.DB) ([]dto.DashServiceStatItemOutput, error) {
+	list := []dto.DashServiceStatItemOutput{}
+	query := tx.SetCtx(public.GetGinTraceContext(c))
+	err := query.Table(t.TableName()).Where("is_delete=0").Select("load_type, count(*) as value").Group("load_type").Scan(&list).Error
+	if err != nil {
+		return nil, err
+	}
+	return list, nil
+}
